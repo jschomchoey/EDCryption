1515c78998c0898864741474af44ee4fb390a3a3
jschomchoey <118211243+jschomchoey@users.noreply.github.com>
2023-09-08 01:28:39 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,48 +1,96 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import filedialog
+from tkinter.filedialog import askopenfile
+import os
 import tkinter as tk
 
 # Screen
 app = tk.Tk()
 app.title("Encryption / Decryption")
-# app.config(bg="skyblue")
+app.config(bg="skyblue")
 
 app.minsize(700, 350)
+app.maxsize(700, 350)
 app.resizable(False, False)
+
 
 # Object
 def print_something():
-    print("Button bind working!")
+    print("Button working!")
 
-# Mode Entry
-mode_frame = Frame(app)
-mode_frame.pack(fill=tk.X)
 
-button = tk.Button(
-    mode_frame, text="Encryption", command=print_something, borderwidth=0, height=2
+def switch_to_encryption_page():
+    notebook.select(encryption_frame)
+
+
+def switch_to_decryption_page():
+    notebook.select(decryption_frame)
+
+
+def openfile_en():
+    file = filedialog.askopenfile(
+        mode="r",
+        filetypes=[
+            ("Text Files", "*.txt"),
+            ("Picture Files", "*.png *.jpg *.jpeg"),
+            ("Video Files", "*.mp4 *.mov"),
+            ("All Files", "*."),
+        ],
+    )
+    if file:
+        path_entry_en.delete(0, END)
+        filepath = os.path.abspath(file.name)
+        path_entry_en.insert(END, str(filepath))
+
+
+def openfile_de():
+    file = filedialog.askopenfile(
+        mode="r",
+        filetypes=[
+            ("Text Files", "*.txt"),
+            ("Picture Files", "*.png *.jpg *.jpeg"),
+            ("Video Files", "*.mp4 *.mov"),
+            ("All Files", "*."),
+        ],
+    )
+    if file:
+        path_entry_de.delete(0, END)
+        filepath = os.path.abspath(file.name)
+        path_entry_de.insert(END, str(filepath))
+
+
+notebook = ttk.Notebook(
+    app,
 )
-button.pack(side="left", fill=tk.X, expand=True)
-button = tk.Button(
-    mode_frame, text="Decryption", command=print_something, borderwidth=0, height=2
+notebook.pack(fill=tk.BOTH, expand=True)
+
+style = ttk.Style()
+style.configure("TNotebook.Tab", padding=(141, 7))
+
+# ----------------------------------- Encryption Part -----------------------------------
+encryption_frame = Frame(
+    notebook,
 )
-button.pack(side="left", fill=tk.X, expand=True)
+notebook.add(
+    encryption_frame,
+    text="Encryption",
+)
 
 # Path Entry
-path_frame = Frame(app)
+path_frame = Frame(encryption_frame)
 path_frame.pack(fill=tk.X, pady=10, padx=20)
 
 Label(path_frame, text="         Path").pack(side="left")
 
-username_entry = Entry(path_frame)
-username_entry.pack(side="left", fill=tk.X, expand=True, padx=10)
+path_entry_en = Entry(path_frame)
+path_entry_en.pack(side="left", fill=tk.X, expand=True, padx=10)
 
-button = tk.Button(
-    path_frame, text="Browse", command=print_something, height=1, width=10
-)
+button = tk.Button(path_frame, text="Browse", command=openfile_en, height=1, width=10)
 button.pack()
 
 # Password Entry
-password_frame = Frame(app)
+password_frame = Frame(encryption_frame)
 password_frame.pack(fill=tk.X, padx=20)
 
 Label(password_frame, text="Password").pack(side="left")
@@ -62,8 +110,8 @@ button = tk.Button(
 button.pack()
 
 # Progress
-progress = Entry(app)
-progress.pack(fill=tk.BOTH,expand=True,pady=10, padx=20)
+progress = Entry(encryption_frame)
+progress.pack(fill=tk.BOTH, expand=True, pady=10, padx=20)
 
 
 # Progress Bar
@@ -72,7 +120,61 @@ def pBar():
 
 
 progBar = ttk.Progressbar(
-    app,
+    encryption_frame,
+    orient=HORIZONTAL,
+    length=700,
+    mode="determinate",
+)
+progBar.pack(pady=15, padx=20)
+
+# ----------------------------------- Decryption Part -----------------------------------
+decryption_frame = Frame(notebook)
+notebook.add(decryption_frame, text="Decryption")
+
+# Path Entry
+path_frame = Frame(decryption_frame)
+path_frame.pack(fill=tk.X, pady=10, padx=20)
+
+Label(path_frame, text="         Path").pack(side="left")
+
+path_entry_de = Entry(path_frame)
+path_entry_de.pack(side="left", fill=tk.X, expand=True, padx=10)
+
+button = tk.Button(path_frame, text="Browse", command=openfile_de, height=1, width=10)
+button.pack()
+
+# Password Entry
+password_frame = Frame(decryption_frame)
+password_frame.pack(fill=tk.X, padx=20)
+
+Label(password_frame, text="Password").pack(side="left")
+
+username_entry = Entry(password_frame)
+username_entry.pack(side="left", fill=tk.X, expand=True, padx=10)
+
+button = tk.Button(
+    password_frame,
+    text="Decryption",
+    command=print_something,
+    height=1,
+    width=10,
+    bg="black",
+    fg="#fff",
+)
+button.pack()
+
+# Progress
+progress = Entry(decryption_frame)
+progress.pack(fill=tk.BOTH, expand=True, pady=10, padx=20)
+
+
+# Progress Bar
+def pBar():
+    progBar["value"] += 10
+
+
+progBar = ttk.Progressbar(
+    decryption_frame,
     orient=HORIZONTAL,
     length=700,
     mode="determinate",
